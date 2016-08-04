a44cf870a4ef31954461c55c4fce26e3c0c08dfa
Max Joseph <maxwellbjoseph@gmail.com>
2016-08-04 09:40:17 -0600

    adding ggplot tutorial

---
+++ R/05-ggplot.R
@@ -0,0 +1,120 @@
+## Introduction to ggplot2 -----------------------------------------------------
+
+# ggplot2 is an R package for visualization that is based on the book "The
+# Grammar of Graphics" by Lelend Wilkinson. The book lays out a conceptual
+# framework for thinking about graphics as layered elements, and the R package
+# ggplot2 implements this concept. ggplot2 provides one common framework to
+# produce a wide variety of plots, rather than base graphics which represents
+# a collection of independent or at best loosely coupled methods.
+
+# Plots emerge from layering the following elements
+# - aesthetics (roles variables play)
+# - geometric objects (e.g., points and lines)
+# - scales
+# - stats
+# - facets
+
+# Like dplyr and tidyr, ggplot2 is designed to operate on data frames.
+library(ggplot2)
+
+# load data
+data(msleep)
+
+# check object class
+class(msleep)
+
+# Aesthetics can be defined at the global level or within geoms
+ggplot(msleep, aes(x = bodywt, y = brainwt)) +
+  geom_point()
+
+ggplot(msleep) +
+  geom_point(aes(x = bodywt, y = brainwt))
+
+# Axes can be easily scaled
+ggplot(msleep) +
+  geom_point(aes(x = bodywt, y = brainwt)) +
+  scale_y_log10() +
+  scale_x_log10()
+
+# Or converted to other coordinate systems
+df <- data.frame(
+  variable = c("does not resemble", "resembles"),
+  value = c(20, 80)
+)
+ggplot(df, aes(x = "", y = value, fill = variable)) +
+  geom_bar(width = 1, stat = "identity") +
+  scale_fill_manual(values = c("red", "yellow")) +
+  coord_polar("y", start = pi / 3) +
+  labs(title = "Pac man")
+
+# Plots can be assigned and modified later
+p <- ggplot(msleep, aes(x = bodywt, y = brainwt)) +
+  geom_point() +
+  scale_y_log10() +
+  scale_x_log10()
+
+# ggplot comes with different themes too
+p + theme_bw()
+p + theme_minimal()
+p + theme_classic()
+p + theme_light()
+
+# you can easily add fitted lines
+p + stat_smooth()
+
+# you can even specify linear and generalized linear models
+p + stat_smooth(method = 'lm', formula = y ~ x + x^2)
+
+# faceting makes panels for each level of a variable
+p +
+  facet_wrap(~vore)
+
+p +
+  facet_grid(order ~ vore) +
+  stat_smooth(method = 'lm')
+
+# color can be added as an aesthetic
+ggplot(msleep, aes(x = bodywt, y = brainwt, color = vore)) +
+  geom_point() +
+  scale_y_log10() +
+  scale_x_log10()
+
+ggplot(msleep, aes(x = bodywt, y = brainwt, color = vore)) +
+  geom_point() +
+  scale_y_log10() +
+  scale_x_log10() +
+  stat_smooth(method = 'lm') +
+  theme_minimal()
+
+# boxplots are trivial
+ggplot(msleep, aes(x = conservation, y = awake)) +
+  geom_boxplot()
+
+# how about with the points?
+ggplot(msleep, aes(x = vore, y = awake)) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_jitter()
+
+# line plots are also easy
+data(co2)
+co2 <- data.frame(co2)
+lp <- ggplot(co2, aes(x = 1:length(co2), y = co2)) +
+  geom_line()
+lp
+
+# Your turn
+#
+# msleep
+#
+# 1) Make a scatterplot of sleep_total vs. sleep_rem
+# 2) Make point size proportional to log(body mass)
+# 3) Add a OLS regression line
+# 4) Color-code the points according to vore. Does the scaling of REM & total sleep differ w/ diet?
+#
+# txhousing
+#
+# 1) Plot a histogram of median house sales prices
+# 2) Plot the overall density of median house sales prices, changing your code as little as possible
+# 3) Plot the density for each city (using color, fill, or facet)
+# 4) Does median house price vary consistently among months or across years? If so, are patterns consistent among cities?
+
